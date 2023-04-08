9d58110a1b57a6fd411691fa5c8b523328a528cb
respuNN <68736280+respuNN@users.noreply.github.com>
2023-04-08 12:08:31 +0300

    Add files via upload

---
+++ important_files/config.py
@@ -0,0 +1,14 @@
+
+#You need to add your server ip and port.
+server_ip = "play.server.net"
+port = "30120"
+serverurl = f"http://{server_ip}:{port}/players.json" #YOU DON'T NEED TO TOUCH THIS.
+
+# Assigning the channel for sending log messages.
+channelid = 123456789123456789
+
+# Separated player identifiers and their associated names
+separated = {
+    "steam:123456789123456": "Separated Player 1",
+    "steam:123456789123456": "Separated Player 2"
+}
